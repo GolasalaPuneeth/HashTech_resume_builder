81139ad00cdc58f6cbcc723102aab01c9b18a764
golasalapuneeth <puneeth3sprime@gmail.com>
2025-06-20 15:34:18 +0000

    Updated with .env changes

--- db.py
+++ db.py
@@ -1,5 +1,4 @@
 import os
-import getpass
 from sqlmodel import SQLModel
 from dotenv_vault import load_dotenv
 from sqlmodel.ext.asyncio.session import AsyncSession
@@ -10,13 +9,13 @@ load_dotenv()
 # DATABASE_URL = "sqlite+aiosqlite:///ResumeBuild.db"
 # if not os.environ.get("DATABASE_URL"):
 #     os.environ["DATABASE_URL"] = getpass.getpass("Enter API key for OpenAI: ")
-
+os.environ["DATABASE_URL"] = os.getenv("DATABASE_URL")
 DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     print("Error: DB URL environment variable not set.")
     raise ValueError("Error: DB URL environment variable not set.")
 
-
+print(DATABASE_URL)
 engine = create_async_engine(DATABASE_URL, echo=True)
 async_session = sessionmaker(
     engine, class_=AsyncSession, expire_on_commit=False
