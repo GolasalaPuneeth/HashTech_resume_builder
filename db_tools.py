8f04c5cba1e8f1ca14cbc16afb382ff3d8b828d0
golasalapuneeth <puneeth3sprime@gmail.com>
2025-06-21 14:59:41 +0000

    updated with User_profile and Streaming api for testing

---
+++ db_tools.py
@@ -0,0 +1,14 @@
+from db import AsyncSession
+from DBModels import User
+from sqlmodel import select
+from fastapi import HTTPException,status
+
+async def get_user_master_data(email:str, session: AsyncSession):
+    try:
+        result = await session.exec(select(User).where(User.email == email.lower()))
+        user = result.first()
+        if user is None:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User not found: ")
+        return user.Master_resune_data
+    except Exception as e:
+        raise e
