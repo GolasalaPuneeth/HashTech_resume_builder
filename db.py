b8f51a64a8d2dadc45300014886a1e4b5e14f7f0
golasalapuneeth <puneeth3sprime@gmail.com>
2025-06-23 13:28:52 +0000

    updated with echo false

--- db.py
+++ db.py
@@ -13,7 +13,7 @@ if not DATABASE_URL:
 
 print(DATABASE_URL)
 engine = create_async_engine(DATABASE_URL, 
-                             echo=True,
+                             #for live updates echo=True,
                              future=True,
                              # Disable prepared statement caching
                              connect_args={"statement_cache_size": 0})
